5f00a98d1f4c21ffef5bebe13d36e666ab050fe0
jja42 <jja42w@gmail.com>
2025-06-30 14:34:45 -0400

    Menu Button Struct

--- menu.h
+++ menu.h
@@ -1,7 +1,20 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include <stdbool.h>
+
+
 void include_test();
+
+typedef struct {
+char* name;
+int x_pos;
+int y_pos;
+int width;
+int height;
+bool interactible;
+void (*on_click)(MenuButton* self);
+} MenuButton;
 
 
 #endif
