c94d110e3fb9ac84cac5054c8ab95389de946804
jja42 <jja42w@gmail.com>
2025-07-07 19:05:56 -0400

    Researching JSON Parsing

--- json_parser.h
+++ json_parser.h
@@ -1,6 +1,13 @@
 #ifndef JSON_PARSER_H
 #define JSON_PARSER_H
 
+typedef struct {
+char* key;
+char* value;
+} JsonObj;
 
+char* read_json_into_buffer();
+
+void read_buffer_into_objects();
 
 #endif
